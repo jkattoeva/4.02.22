0dc2f5adf9168f481007e45cfbb66b1c7bb0cc9f
Janetta Kataoeva <jkattoeva@gmail.com>
2022-02-04 14:44:50 +0600

    worked with index.html and changed text colors in script.js

--- script.js
+++ script.js
@@ -13,11 +13,20 @@ var vasya = {
   }
 }
 
-vasya.greet();
-vasya.hobbies.display();
+// vasya.greet();
+// vasya.hobbies.display();
 
 // 1. Call 'greet' method multiple times.
 
 // 2. Include age in the greeting.
 // 2. Try changing name and age.
 // 4. Create method 'bye'.
+
+
+
+// document.body.style.background = "red"; 
+// document.body.textContent = "Hello world";
+
+document.getElementById("hello-world").textContent = "Nice to meet you!";
+document.getElementById("hello-world").style.color = "white";
+document.getElementById("hello-world").style.background = "blue";
