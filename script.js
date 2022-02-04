403f0c9872dfcb6a05e9932e0f95373c1916886b
Janetta Kataoeva <jkattoeva@gmail.com>
2022-02-04 15:59:37 +0600

    styled elemnts with javascript

--- script.js
+++ script.js
@@ -27,6 +27,54 @@ var vasya = {
 // document.body.style.background = "red"; 
 // document.body.textContent = "Hello world";
 
-document.getElementById("hello-world").textContent = "Nice to meet you!";
-document.getElementById("hello-world").style.color = "white";
-document.getElementById("hello-world").style.background = "blue";
+// document.getElementById("hello-world").textContent = "Nice to meet you!";
+// document.getElementById("hello-world").style.color = "white";
+// document.getElementById("hello-world").style.background = "blue";
+
+
+// getElementById 
+let helloWorldDiv = document.getElementById("hello-world");
+
+helloWorldDiv.textContent = "Nice to meet you!";
+helloWorldDiv.style.background = "green";
+helloWorldDiv.style.color = "white";
+helloWorldDiv.style.textDecoration = "underline";
+helloWorldDiv.style.fontSize = "28px";
+
+
+// getElementByTagName
+let allParagraphs =  document.getElementsByTagName("P");
+
+for (let paragraph of allParagraphs) {
+  paragraph.style.backgroundColor = "blue";
+  paragraph.style.color = "yellow";
+  paragraph.style.fontWeight = "bold";
+  paragraph.style.fontSize = "18px";
+
+}
+
+//getElementsByClassName
+// italic
+
+//  document.getElementsByClassName("hello-world");
+
+// // for (let helloWorld of /*allHelloWorlds*/) {
+// //   helloWorld.style.backgroundColor = "yellow";
+// //   helloWorld.style.color = "red";
+// //   helloWorld.style.fontStyle = "italic";
+  
+// }
+//querySelector
+let helloWorld2 = document.querySelector("#hello-world");
+helloWorld2.style.backgroundColor = "purple";
+
+//querySelectorAll
+let allHelloWorlds2 = document.querySelectorAll(".hello-world");
+
+for (let helloWorld of allHelloWorlds2) {
+  helloWorld2.style.fontSize = "15px"
+}
+
+document.querySelector('p:first-of-type').textContent = "HELLO";
+
+
